f86ea561afffb90bd3182790ddf89231ba66764a
Henrique Schmidt <henrique.rieger@gmail.com>
2023-02-13 15:49:26 -0300

    fix: remove blog configs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,13 +43,6 @@ const config = {
           editUrl:
             'https://github.com/libercapital/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
         theme: {
           customCss: [
             require.resolve('./src/css/fonts.css'),
@@ -144,17 +137,13 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/libercapital',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Liber Capital.`,
       },
       prism: {
         theme: lightCodeTheme,
