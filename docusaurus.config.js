334310db4bff09a157f04adeaf3874f870f2f482
Henrique Schmidt <henrique.rieger@gmail.com>
2023-02-13 16:29:53 -0300

    fix: base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'hhttps://libercapital.com.br',
+  url: 'hhttps://libercapital.com.br/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/developers-website/',
