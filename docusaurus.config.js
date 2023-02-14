de16ff739c1e1176b20a55cedcf40b59164dc1c1
Henrique Schmidt <henrique.rieger@gmail.com>
2023-02-14 10:38:55 -0300

    feat: add cname

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,10 +10,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://libercapital.com.br/',
+  url: 'https://developers.libercapital.com.br',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/developers-website/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
